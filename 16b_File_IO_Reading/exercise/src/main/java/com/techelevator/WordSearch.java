baa17d91095d4e94e33899e42b6351f02d3b343f
Mason <astinch09@gmail.com>
2022-04-02 14:12:20 -0400

    word search works

--- 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,47 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordSearch {
 
 	public static void main(String[] args) {
+		int lineNumber = 1;
+		boolean isCaseSensitive = true;
+		Scanner userInput = new Scanner(System.in);
 
+		System.out.println("What is the name of the file that should be searched?");
+		String pathName = userInput.nextLine();
+		File filePathing = new File(pathName);
+
+		System.out.println("What is the word you are looking for?");
+		String wordSearched = userInput.nextLine();
+		System.out.println("Should the search be cAsE sEnsItIve? (y/n) : ");
+		String caseSensitivityChoice = userInput.nextLine();
+
+		if (caseSensitivityChoice.equalsIgnoreCase("y")){
+			isCaseSensitive = true;
+		}
+
+		try (Scanner dataInput = new Scanner(filePathing)) {
+
+			while (dataInput.hasNextLine()) {
+				String line = dataInput.nextLine();
+				String[] s = line.split(" ");
+
+				for (String words : s) {
+					if (!isCaseSensitive && words.equalsIgnoreCase(wordSearched)) {
+						System.out.println(lineNumber + " " + line);}
+
+					if (isCaseSensitive && words.contains(wordSearched))
+						System.out.println(lineNumber + " " + line);}
+
+				lineNumber++;
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("The file was not found: " + filePathing.getAbsolutePath());
+		}
 	}
-
 }
